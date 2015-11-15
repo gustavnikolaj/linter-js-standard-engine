b1aa6eea0b73ab7079332ff111de700ec2d3d3cb
Gustav Nikolaj Olsen <gno@one.com>
2015-11-15 15:27:33 +0100

    initial commit

---
+++ init.js
@@ -0,0 +1,28 @@
+var generateLintPromise = require('./lib/generateLintPromise')
+
+module.exports = {
+  provideLinter: function () {
+    return {
+      name: 'lint',
+      grammarScopes: ['source.js', 'source.js.jsx'],
+      scope: 'file',
+      lintOnFly: true,
+      lint: function (textEditor) {
+        var fileContent = textEditor.getText()
+        var filePath = textEditor.getPath()
+        var fileScope = textEditor.getGrammar().scopeName
+
+        if (this.grammarScopes.indexOf(fileScope) < 0) {
+          return []
+        }
+
+        return generateLintPromise(filePath, fileContent).catch(function (err) {
+          atom.notifications.addError('Something bad happened', {
+            detail: err.stack,
+            dismissable: true
+          })
+        })
+      }
+    }
+  }
+}
