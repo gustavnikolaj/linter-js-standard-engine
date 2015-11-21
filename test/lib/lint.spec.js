57b088fbe0e00d3ef9f9af767a1253b3306c16d7
Gustav Nikolaj Olsen <gno@one.com>
2015-11-21 10:29:01 +0100

    add tests for lib/lint.js

---
+++ test/lib/lint.spec.js
@@ -0,0 +1,83 @@
+/* global describe, it */
+
+var expect = require('unexpected').clone()
+var lintFactory = require('../../lib/lint')
+
+describe('lib/lint.js', function () {
+  it('should return a function when called with path and content', function () {
+    return expect(lintFactory('/filePath', 'var foo = bar'), 'to be a function')
+  })
+  it('should call the linters lintText method', function () {
+    var lint = lintFactory('/filePath', 'var foo = bar')
+    var linterMock = {
+      lintText: function (fileContent, cb) {
+        return cb(new Error('MockError'))
+      }
+    }
+    return expect(lint(linterMock), 'to be rejected').then(function (err) {
+      return expect(err, 'to have message', 'MockError')
+    })
+  })
+  it('should convert a eslint report to an atom report', function () {
+    var lint = lintFactory('/filePath', 'var foo = bar')
+    var linterMock = {
+      lintText: function (fileContent, cb) {
+        var output = {
+          results: [ { filePath: '<text>', messages: [
+            {
+              ruleId: 'eol-last',
+              severity: 2,
+              message: 'Newline required at end of file but not found.',
+              line: 1,
+              column: 2,
+              nodeType: 'Program',
+              source: 'var foo = "bar"',
+              fix: { range: [15, 15], text: '\n' }
+            },
+            {
+              ruleId: 'no-unused-vars',
+              severity: 2,
+              message: '"foo" is defined but never used',
+              line: 1,
+              column: 5,
+              nodeType: 'Identifier',
+              source: 'var foo = "bar"'
+            },
+            {
+              ruleId: 'quotes',
+              severity: 2,
+              message: 'Strings must use singlequote.',
+              line: 1,
+              column: 11,
+              nodeType: 'Literal',
+              source: 'var foo = "bar"',
+              fix: { range: [10, 15], text: "'bar'" }
+            }
+          ], errorCount: 3, warningCount: 0 } ], errorCount: 3, warningCount: 0 }
+        return cb(null, output)
+      }
+    }
+    return expect(lint(linterMock), 'to be fulfilled').then(function (report) {
+      return expect(report, 'to equal', [
+        {
+          type: 'Warning',
+          text: 'Newline required at end of file but not found.',
+          filePath: '/filePath',
+          range: [ [ 0, 0 ], [ 0, 1 ] ]
+        },
+        {
+          type: 'Warning',
+          text: '"foo" is defined but never used',
+          filePath: '/filePath',
+          range: [ [ 0, 1 ], [ 0, 4 ] ]
+        },
+        {
+          type: 'Warning',
+          text: 'Strings must use singlequote.',
+          filePath: '/filePath',
+          range: [ [ 0, 1 ], [ 0, 10 ] ]
+        }
+      ])
+    })
+  })
+})
