ec776c2b390674c85ed86bf7cd4661b68724eb3d
Gustav Nikolaj Olsen <gno@one.com>
2015-12-21 21:13:02 +0100

    add support for nested packages
    fixes #6
    
    done by continueing searching for package.json files, if one is
    found without a valid linter.

--- test/lib/findOptions.spec.js
+++ test/lib/findOptions.spec.js
@@ -1,6 +1,9 @@
 /* global describe, it */
 
-var expect = require('unexpected').clone()
+var expect = require('unexpected')
+  .clone()
+  .use(require('unexpected-fs'))
+var path = require('path')
 var findOptions = require('../../lib/findOptions')
 
 describe('lib/findOptions', function () {
@@ -14,5 +17,25 @@ describe('lib/findOptions', function () {
         }
       })
     })
+  })
+  it('should be able to find options about this module in a nested module', function () {
+    var mockedFileSystem = {}
+    var fixturesPath = path.resolve(__dirname, '../some-module')
+    mockedFileSystem[fixturesPath] = {
+      'package.json': '{}',
+      'index.js': 'module.exports = "double quotes!!!"'
+    }
+    var fileName = path.resolve(fixturesPath, 'index.js')
+    return expect(function () {
+      return expect(findOptions(fileName), 'to be fulfilled').then(function (options) {
+        return expect(options, 'to satisfy', {
+          linter: 'standard',
+          pathToLinter: /node_modules\/standard$/,
+          options: {
+            globals: [ 'atom' ]
+          }
+        })
+      })
+    }, 'with fs mocked out', mockedFileSystem, 'not to error')
   })
 })
