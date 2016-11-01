717f8ecc061b60db7f680a7568d6c78c06bab471
Mark Wubben <mark@novemberborn.net>
2016-11-01 13:37:37 +0000

    Refactor atomHelper
    Log added information for future assertions. Allow promise rejections to
    be silenced, while still reporting unexpected errors.

--- test/util/atomHelper.js
+++ test/util/atomHelper.js
@@ -1,10 +1,24 @@
 if (typeof global.atom === 'undefined') {
   global.atom = {
     notifications: {
+      _errors: [],
+
       addError: function (desc, obj) {
-        return new Promise(function (resolve, reject) {
-          reject(obj.error)
+        const promise = new Promise((resolve, reject) => {
+          // Reject using setImmediate, allowing tests to silence the error
+          // before it's reported has an unhandled rejection.
+          setImmediate(() => reject(obj.error))
         })
+
+        this._errors.push({
+          desc,
+          obj,
+          silence () {
+            promise.catch(() => {})
+          }
+        })
+
+        return promise
       }
     }
   }
