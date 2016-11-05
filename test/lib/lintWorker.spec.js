gustavnikolaj/linter-js-standard-engine

commit 485b28865cbc7741e909e1ea3301f4da2120f920

pass linterName to worker and require it directly (#22)
* pass linterName to worker and require it directly

as the lint worker is running in the project folder require works
as expected, and any resolving tricks can be ignored.

* simpler args handling

lib/lintWorker.js
@@ -1,6 +1,5 @@
-var lintPath = process.argv.slice(2).pop()
-
-var linter = require(lintPath)
+var linterName = process.argv[2]
+var linter = require(linterName)
 
 process.on('message', function (data) {
   linter.lintText(data.source, function (err, result) {

init.js
@@ -34,7 +34,7 @@ module.exports = {
             if (fileIsIgnored) {
               return [] // No errors
             }
-            return getLinter(options.pathToLinter, options.projectRoot)
+            return getLinter(options.linter, options.projectRoot)
               .then(lint(filePath, fileContent))
           })
           .catch(function (err) {

lib/findOptions.js
@@ -1,6 +1,5 @@
 var path = require('path')
 var fs = require('fs')
-var resolveFrom = require('resolve-from')
 var supportedLinters = require('./supportedLinters')
 
 function firstPackageJson (filePath) {
@@ -58,11 +57,6 @@ function readOptionsFromPackageJson (packageJsonPath) {
 
       var linter = linters[0]
       var pathToProject = path.dirname(packageJsonPath)
-      var pathToLinter = resolveFrom(pathToProject, linter)
-
-      if (!pathToLinter) {
-        return reject(new Error('linter ' + linter + ' does not seem to be installed'))
-      }
 
       // Support scoped packages. Assume their `cmd` (and thus options key) is
       // configured as the package name *without* the scope prefix.
@@ -74,7 +68,6 @@ function readOptionsFromPackageJson (packageJsonPath) {
       resolve({
         linter: linter,
         projectRoot: pathToProject,
-        pathToLinter: pathToLinter,
         options: packageJson[optionsKey] || {}
       })
     })

lib/getLinter.js
@@ -4,15 +4,15 @@ var lruCache = require('lru-cache')
 
 var linters = lruCache({
   max: 2,
-  dispose: function (pathToLinter, linter) {
+  dispose: function (cacheKey, linter) {
     linter.shutdown()
   }
 })
 
 var lintWorkerPath = path.resolve(__dirname, 'lintWorker.js')
 
-var createLinter = function (pathToLinter, projectRoot) {
-  var child = fork(lintWorkerPath, [ pathToLinter ], {
+var createLinter = function (linterName, projectRoot) {
+  var child = fork(lintWorkerPath, [ linterName ], {
     cwd: projectRoot
   })
   var sequenceNumber = 0
@@ -46,13 +46,13 @@ var createLinter = function (pathToLinter, projectRoot) {
   }
 }
 
-module.exports = function getLinter (pathToLinter, projectRoot) {
+module.exports = function getLinter (linterName, projectRoot) {
   return new Promise(function (resolve, reject) {
-    var cacheKey = pathToLinter + '\n' + projectRoot
+    var cacheKey = linterName + '\n' + projectRoot
     var linter = linters.get(cacheKey)
 
     if (!linter) {
-      linter = createLinter(pathToLinter, projectRoot)
+      linter = createLinter(linterName, projectRoot)
       linters.set(cacheKey, linter)
     }
 

test/lib/findOptions.spec.js
@@ -13,7 +13,6 @@ describe('lib/findOptions', function () {
     return expect(findOptions(__filename), 'to be fulfilled').then(function (options) {
       return expect(options, 'to satisfy', {
         linter: 'standard',
-        pathToLinter: require.resolve('standard'),
         options: {
           globals: [ 'atom' ]
         }
@@ -27,7 +26,6 @@ describe('lib/findOptions', function () {
         return expect(options, 'to equal', {
           projectRoot: fixturesPath('simpleSemiStandard'),
           linter: 'semistandard',
-          pathToLinter: fixturesPath('simpleSemiStandard/node_modules/semistandard/index.js'),
           options: {}
         })
       })
@@ -40,7 +38,6 @@ describe('lib/findOptions', function () {
         return expect(options, 'to equal', {
           projectRoot: fixturesPath('scopedLinter'),
           linter: '@novemberborn/as-i-preach',
-          pathToLinter: fixturesPath('scopedLinter/node_modules/@novemberborn/as-i-preach/index.js'),
           options: {}
         })
       })

commit fb61700856b5df3f3e563c2321a629cf5aefbc7d

Fix linter resolution
Linters must be resolved from the project root, not the `lintWorker`
module. Fixes regression introduced in #22.

The previous behavior accidentally triggered code paths that did not
have explicit test coverage. Add new tests and fix tests and fixtures
that never quite worked but still passed.

lib/lintWorker.js
@@ -1,3 +1,5 @@
+var resolveCwd = require('resolve-cwd')
+
 var linterName = process.argv[2]
 
 function generateFakeLintResult (msg) {
@@ -10,20 +12,26 @@ function getLinter () {
     return linterInstance
   }
 
-  try {
-    linterInstance = require(linterName)
-    return linterInstance
-  } catch (e) {
-    return {
-      lintText: function (source, callback) {
-        if (e.code === 'MODULE_NOT_FOUND') {
-          var message = 'Could not load linter "' + linterName + '"'
-          return callback(null, generateFakeLintResult(message))
+  var linterPath = resolveCwd(linterName)
+  if (linterPath) {
+    try {
+      linterInstance = require(linterPath)
+    } catch (err) {
+      linterInstance = {
+        lintText (source, callback) {
+          callback(err)
         }
-        callback(e.stack || e.message)
+      }
+    }
+  } else {
+    linterInstance = {
+      lintText (source, callback) {
+        return callback(null, generateFakeLintResult('Could not load linter "' + linterName + '"'))
       }
     }
   }
+
+  return linterInstance
 }
 
 process.on('message', function (data) {

test/init.spec.js
@@ -48,7 +48,7 @@ describe('linter-js-standard-engine', function () {
       path: filePath
     })
     return expect(lint(textEditor), 'to be fulfilled').then(function (data) {
-      return expect(data, 'to be a valid lint report')
+      return expect(data, 'to be empty')
     })
   })
   it('should clean linters when deactivated', () => {

test/lib/lint.spec.js
@@ -77,6 +77,10 @@ describe('lib/lint.js', function () {
                   nodeType: 'Literal',
                   source: 'var foo = "bar"',
                   fix: { range: [10, 15], text: "'bar'" }
+                },
+                {
+                  fatal: true,
+                  message: 'Made up message to test fallback code paths'
                 }
               ],
               errorCount: 3,
@@ -108,6 +112,12 @@ describe('lib/lint.js', function () {
           text: 'Strings must use singlequote.',
           filePath: '/filePath',
           range: [ [ 0, 1 ], [ 0, 10 ] ]
+        },
+        {
+          type: 'Error',
+          text: 'Made up message to test fallback code paths',
+          filePath: '/filePath',
+          range: [ [ 0, 0 ], [ 0, 0 ] ]
         }
       ])
     })

test/lib/lintWorker.spec.js
@@ -2,6 +2,7 @@
 
 const expect = require('unexpected')
 const childProcess = require('child_process')
+const path = require('path')
 
 const workerPath = require.resolve('../../lib/lintWorker')
 
@@ -35,7 +36,45 @@ describe('lib/lintWorker', () => {
     child.send({ id: 1, source: '' })
 
     return promise.then((message) => {
-      expect(message.err, 'to be', 'crash in linter loading')
+      expect(message.err, 'to have property', 'message', 'crash in linter loading')
+    })
+  })
+  it('should report missing linters as a linter message', () => {
+    const child = childProcess.fork(workerPath, ['non-existent-for-sure-or-so-we-hope'])
+    const promise = new Promise(resolve => {
+      child.on('message', m => resolve(m))
+    })
+
+    child.send({ id: 1, source: '' })
+
+    return promise.then((message) => {
+      expect(message, 'to satisfy', {
+        result: {
+          results: [
+            {
+              messages: [
+                {
+                  message: 'Could not load linter "non-existent-for-sure-or-so-we-hope"',
+                  fatal: true
+                }
+              ]
+            }
+          ]
+        }
+      })
+    })
+  })
+  it('should resolve linter from working directory', () => {
+    const cwd = path.resolve(__dirname, '..', 'fixtures', 'localLinter')
+    const child = childProcess.fork(workerPath, ['my-linter'], { cwd })
+    const promise = new Promise(resolve => {
+      child.on('message', m => resolve(m))
+    })
+
+    child.send({ id: 1, source: '' })
+
+    return promise.then((message) => {
+      expect(message.result, 'to have property', 'hello', 'world')
     })
   })
 })