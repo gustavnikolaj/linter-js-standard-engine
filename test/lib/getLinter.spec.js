415ac432b804bacd9dc45e362530d41a669cfb42
Mark Wubben <mark@novemberborn.net>
2016-11-05 16:59:05 +0000

    Remove workers from cache when they exit
    Ensures crashed workers are not used for new lint invocations.

--- test/lib/getLinter.spec.js
+++ test/lib/getLinter.spec.js
@@ -81,9 +81,48 @@ describe('lib/getLinter', () => {
     getLinter('linter', '/')
     forks.linter.emit('message', { id: 10 })
   })
+  it('should clean up linters that exit', () => {
+    return getLinter('first', '/').then(first => {
+      return expect(getLinter('first', '/'), 'when fulfilled', 'to be', first)
+        .then(() => {
+          forks.first.emit('exit')
+          return new Promise(resolve => setTimeout(resolve, 10))
+        })
+        .then(() => {
+          const old = first
+          return getLinter('first', '/').then(first => {
+            expect(first, 'not to be', old)
+
+            // Ensure first is purged from the cache
+            getLinter('second', '/')
+            getLinter('third', '/')
+
+            const child = forks.first
+            expect(child, 'to have property', 'wasDisconnected', true)
+            const promise = getLinter('first', '/')
+
+            child.emit('exit')
+            return new Promise(resolve => setTimeout(resolve, 10))
+              .then(() => promise)
+              .then(first => {
+                return expect(getLinter('first', '/'), 'when fulfilled', 'to be', first)
+              })
+          })
+        })
+    })
+  })
+  it('should ignore disconnect errors', () => {
+    return getLinter('first', '/').then(linter => {
+      forks.first.disconnect = () => { throw new Error('ignore me') }
+      expect(() => linter.shutdown(), 'not to throw')
+    })
+  })
 
   describe('cleanLinters()', () => {
     it('should shut down all workers', () => {
+      // Reset state
+      getLinter.cleanLinters()
+
       getLinter('first', '/')
       getLinter('second', '/')
       expect(forks, 'to satisfy', {
